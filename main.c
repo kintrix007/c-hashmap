215e8ab7dc9cb241b7bd8df3bb544ebc660775a1
kintrix007 <boldizsar02@gmail.com>
2022-11-30 13:30:34 +0100

    Improved the example usage in main.c

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 
 #include "hashmap.h"
 
+static void my_print_thingy(char *key, void *value);
+
 int main() {
-    struct HashMap *hm = hm_new(1);
+    struct HashMap *hm = hm_new(4);
 
     int *val = malloc(sizeof(int));
     *val = 5;
@@ -15,24 +17,45 @@ int main() {
     val = malloc(sizeof(int));
     *val = 7;
     hm_set(hm, "Mark", val);
-    hm_print(hm, NULL);
     val = malloc(sizeof(int));
     *val = 9999;
     hm_set(hm, "Ur mom", val);
 
-    // hm_print(hm, NULL);
+    printf("\n");
 
-    // void *value = hm_get(hm, "Joe");
-    // if (value != NULL) printf("Joe: %d\n", *(int*)value);
-    // value = hm_get(hm, "Lucas");
-    // if (value != NULL) printf("Lucas: %d\n", *(int *)value);
+    hm_print(hm, NULL);
 
-    // value = hm_remove(hm, "Joe");
-    // if (value != NULL) free(value);
-    // value = hm_remove(hm, "Robert");
-    // if (value != NULL) free(value);
+    printf("\n");
 
-    // hm_print(hm, NULL);
+    void *value = hm_get(hm, "Joe");
+    if (value != NULL) printf("-> Joe: %d\n", *(int*)value);
+    else printf("-> Joe: (nil)\n");
+
+    value = hm_get(hm, "Lucas");
+    if (value != NULL) printf("-> Lucas: %d\n", *(int*)value);
+    else printf("-> Lucas: (nil)\n");
+
+    printf("\n");
+
+    hm_foreach(hm, my_print_thingy);
+
+    printf("\n");
+
+    value = hm_remove(hm, "Joe");
+    if (value != NULL) free(value);
+    value = hm_remove(hm, "Robert");
+    if (value != NULL) free(value);
+
+    hm_print(hm, NULL);
 
     hm_free(hm, free);
+}
+
+static void my_print_thingy(char *key, void* value) {
+    printf("Foreach => ");
+    if (value == NULL) {
+        printf("%s : %p\n", key, value);
+    } else {
+        printf("%s : %d\n", key, *(int*)value);
+    }
 }
